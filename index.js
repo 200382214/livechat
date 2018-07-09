4e3dc4dfb32e97e356c7f0c23ba16b6e28073860
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2018-07-09 01:04:31 -0400

    index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 8080;
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
